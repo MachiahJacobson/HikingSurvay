8cc7f83c98a4a35ecc07b2009cc35b1b19827faa
Mac <macjacobson99@gmail.com>
2025-12-10 14:18:01 -0700

    Initial Commit

---
+++ HikingSurvay/HikingSurvayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurvayApp.swift
+//  HikingSurvay
+//
+//  Created by Jacobson, Machiah - Student on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurvayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikingSurvayTests/HikingSurvayTests.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurvayTests.swift
+//  HikingSurvayTests
+//
+//  Created by Jacobson, Machiah - Student on 12/10/25.
+//
+
+import Testing
+@testable import HikingSurvay
+
+struct HikingSurvayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
